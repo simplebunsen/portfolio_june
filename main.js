458be6d84e48aab467f7bcedf7fc55745a780bf4
simplebunsen <52760596+simplebunsen@users.noreply.github.com>
2021-07-14 17:31:31 +0200

    improve menu behaviour

--- main.js
+++ main.js
@@ -2,12 +2,24 @@ const sidebarMenuButton = document.querySelector("#nav-symbol .icon");
 const sidebarMenuCloseButton = document.querySelector("#nav-content .close-btn")
 const sidebarMenuContent = document.querySelector("#nav-content");
 
+const allLinks = document.querySelectorAll("#nav-content nav ul li a");
+
 
 sidebarMenuButton.addEventListener("click", ()=>{
   sidebarMenuContent.classList.add("unfold");
   document.body.style.overflow="hidden";
 });
 sidebarMenuCloseButton.addEventListener("click", ()=>{
+  closeMenu();
+})
+
+allLinks.forEach( link => {
+  link.addEventListener("click", ()=>{
+    closeMenu();
+  })
+})
+
+function closeMenu(){
   sidebarMenuContent.classList.remove("unfold");
   document.body.style.overflow="auto";
-})
+}
